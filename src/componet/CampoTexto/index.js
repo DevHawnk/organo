8ba38fb593e8d896f8e6879fb70351d4a7400b41
DevHawnk <wesley.trabalho42@outlook.com>
2023-10-18 02:19:35 -0300

    atualizado

---
+++ src/componet/CampoTexto/index.js
@@ -0,0 +1,24 @@
+import "./CampoTexto.css";
+
+const CampoTexto = (props) => {
+  let valor = "";
+
+  const aoDigitado = (evento) => {
+    valor = evento.target.value;
+    console.log(valor);
+  };
+
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input
+        value={valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
+};
+
+export default CampoTexto;
