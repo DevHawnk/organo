223ae72cad9252c6b8b87efa1e6ace74d3961abb
wesley souza pereira <103498566+DevHawnk@users.noreply.github.com>
2023-10-31 17:20:15 -0300

    Update index.jsx

--- src/componet/ListaSuspensa/index.jsx
+++ src/componet/ListaSuspensa/index.jsx
@@ -7,10 +7,11 @@ const ListaSuspensa = (props) => {
     <div className="lista-suspensa">
       <label>{props.label}</label>
       <select
-        onChange={(evento) => props.aoAlterado(evento.target.value)}
+        onChange={evento => props.aoAlterado(evento.target.value)}
         required={props.required}
         value={props.value}
       >
+        <option value="">Selecione...</option>
         {props.itens.map((item) => {
           return <option key={item}>{item}</option>;
         })}
