7acbeb2b52b85ad06fffe6030b7b184d52ba6005
wesley souza pereira <103498566+DevHawnk@users.noreply.github.com>
2023-10-31 16:53:30 -0300

    Update Banner.jsx

--- src/componet/Banner/Banner.jsx
+++ src/componet/Banner/Banner.jsx
@@ -4,7 +4,7 @@ export const Banner = () => {
   return (
     <header class="banner">
       <img
-        src="https://scontent.fcaw3-1.fna.fbcdn.net/v/t39.30808-6/301889657_629594195543552_56459218674924060_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=QhuK0WnVfzMAX-QNG30&_nc_ht=scontent.fcaw3-1.fna&oh=00_AfDtLI99ZehwGM6e2kVf4ip376OVPGi3kFWncKQMMxUtqA&oe=653DAD92"
+        src="https://2kolf.com/wp-content/uploads/2020/10/nfl-sunday-ticket-banner-S20.jpg"
         alt="O banner principal da página do card"
       />
     </header>
