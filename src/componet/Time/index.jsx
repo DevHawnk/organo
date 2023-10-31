985e72ab89503fbc59b09529462e6697651235fa
wesley souza pereira <103498566+DevHawnk@users.noreply.github.com>
2023-10-31 16:52:39 -0300

    Update index.jsx

--- src/componet/Time/index.jsx
+++ src/componet/Time/index.jsx
@@ -4,18 +4,18 @@ import "./Time.css";
 const Time = (props) => {
   const css = { backgroundColor: props.corSecundaria };
 
-  return props.atletas > 0 ? (
+  return props.atleta > 0 ? (
     <section className="time" style={css}>
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
 
       <div className="atleta">
         {props.atletas.map((atleta) => (
           <Atleta
-            corDeFundo={props.corPrimaria}
             key={atleta.nome}
             nome={atleta.nome}
             cargo={atleta.cargo}
             imagem={atleta.imagem}
+            corDeFundo={props.corPrimaria}
           />
         ))}
       </div>
