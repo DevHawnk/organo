0f8ee6b1905557ecc9797cb59a5acaf82c285a58
wesley souza pereira <103498566+DevHawnk@users.noreply.github.com>
2023-11-01 12:28:16 +0000

    commit v1.5

--- src/componet/Atleta/index.jsx
+++ src/componet/Atleta/index.jsx
@@ -1,17 +1,21 @@
 import "./Atleta.css";
-import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 const Atleta = ({ atleta, corDeFundo, aoDeletar, aoFavoritar }) => {
   return (
-    <div className="atleta">
+    <div className="colaborador">
       <AiFillCloseCircle
         size={25}
         className="deletar"
         onClick={() => aoDeletar(atleta.id)}
       />
-      <img src={atleta.imagem} alt={atleta.nome} />
-      <h4>{atleta.nome}</h4>
-      <h5>{atleta.cargo}</h5>
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={atleta.imagem} alt={atleta.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{atleta.nome}</h4>
+        <h5>{atleta.cargo}</h5>
+      </div>
     </div>
   );
 };
--- src/componet/Banner/index.jsx
+++ src/componet/Banner/index.jsx
@@ -2,7 +2,7 @@ import "./Banner.css";
 
 const Banner = () => {
   return (
-    <header className="cabecalho">
+    <header className="banner">
       <img
         src="https://2kolf.com/wp-content/uploads/2020/10/nfl-sunday-ticket-banner-S20.jpg"
         alt="Logo do nosso card"
